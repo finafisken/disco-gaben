0ba31d14d9747fe89d54283affc9b170c4d2215e
Markus Warne <markus.warne@gmail.com>
2019-01-22 22:55:20 +0100

    event list and event join

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,11 @@ extern crate chrono;
 extern crate typemap;
 
 use serenity::client::Client;
+use serenity::client::Context;
 use serenity::prelude::EventHandler;
 use serenity::framework::standard::StandardFramework;
 use serenity::model::user::User;
+use serenity::model::channel::Reaction;
 use chrono::prelude::*;
 use typemap::Key;
 use std::env;
@@ -19,12 +21,15 @@ struct Event {
     id: u64,
     date: DateTime<Utc>,
     title: String,
-    desc: String,
     link: String,
     participants: Vec<User>,
 }
 
-impl EventHandler for Handler {}
+impl EventHandler for Handler {
+    fn reaction_add(&self, ctx: Context, reaction: Reaction) { 
+        println!("{} added reaction to {}", reaction.user_id.as_u64(), reaction.message_id.as_u64()) 
+    }
+}
 
 struct EventList;
 
@@ -47,7 +52,9 @@ fn main() {
         .configure(|c| c.prefix("!")) // set the bot's prefix to "!"
         .group("Events", |g| g
             .prefix("event")
-            .cmd("add", add)
+            .cmd("add", event_add)
+            .cmd("list", event_list)
+            .cmd("join", event_join)
         ));
 
     // start listening for events by starting a single shard
@@ -56,7 +63,7 @@ fn main() {
     }
 }
 
-command!(add(ctx, message, args) {
+command!(event_add(ctx, message, args) {
     let mut data = ctx.data.lock();
     let events = data.get_mut::<EventList>().unwrap();
     let id = events.len() as u64 + 1;
@@ -64,10 +71,30 @@ command!(add(ctx, message, args) {
                 id,
                 date: Utc.datetime_from_str(&args.single::<String>().unwrap(), "%Y-%m-%dT%H:%M").unwrap(),
                 title: args.single_quoted::<String>().unwrap(),
-                desc: args.single_quoted::<String>().unwrap(),
                 link: args.single::<String>().unwrap(),
                 participants: vec![message.author.clone()]
             };
     println!("{:?}", event);
     events.insert(id, event);
+    let _ = message.reply("Added event");
+});
+
+command!(event_list(ctx, message) {
+    let mut data = ctx.data.lock();
+    let events = data.get_mut::<EventList>().unwrap();
+
+    for (_, event) in events.iter() {
+        let user_names: Vec<&String> = event.participants.iter().map(|u| &u.name).collect();
+        let msg = format!(":date: **{}** [#{}]\n`{}`\n{}\n```{:?}```", event.title, event.id, event.date, event.link, user_names);
+        let _ = message.channel_id.say(&msg);
+    }
+});
+
+command!(event_join(ctx, message, args) {
+    let mut data = ctx.data.lock();
+    let events = data.get_mut::<EventList>().unwrap();
+
+    let event = events.get_mut(&args.single::<u64>().unwrap()).unwrap();
+    event.participants.push(message.author.clone());
+    let _ = message.reply("Added you to event");
 });
