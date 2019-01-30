bbe974f72104b111057140829d0d74dfcc01419e
Markus Warne <markus.warne@gmail.com>
2019-01-30 20:36:50 +0100

    adapt add event command to work with db

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,10 @@ extern crate rusoto_dynamodb;
 extern crate uuid;
 
 use uuid::Uuid;
-use serenity::client::Client;
-use serenity::prelude::EventHandler;
-use serenity::framework::standard::StandardFramework;
-use serenity::model::user::User;
+use serenity::{client::Client, prelude::EventHandler, framework::standard::StandardFramework, model::user::User};
 use chrono::prelude::*;
 use typemap::Key;
-use std::env;
-use std::collections::HashMap;
-use std::default::Default;
+use std::{env, collections::HashMap, default::Default};
 use rusoto_core::Region;
 use rusoto_dynamodb::{DynamoDb, DynamoDbClient, AttributeValue, PutItemInput};
 
@@ -32,65 +27,25 @@ struct Event {
     participants: Vec<User>,
 }
 
-struct EventList;
+struct DbClient;
 
-impl Key for EventList {
-    type Value = HashMap<u64, Event>;
+impl Key for DbClient {
+    type Value = DynamoDbClient;
 }
 
 fn main() {
     // load .env file
     kankyo::load().expect("Failed to load .env file");
 
-    let mut mock_event: HashMap<String, AttributeValue> = HashMap::new();
-
-    mock_event.insert(String::from("id"), AttributeValue {
-        s: Some(Uuid::new_v4().to_string()),
-        ..Default::default()
-    });
-
-    mock_event.insert(String::from("title"), AttributeValue {
-        s: Some(String::from("Testing")),
-        ..Default::default()
-    });
-
-    mock_event.insert(String::from("link"), AttributeValue {
-        s: Some(String::from("https://www.pcgamer.com")),
-        ..Default::default()
-    });
-
-    mock_event.insert(String::from("date"), AttributeValue {
-        s: Some(String::from("2019-10-10T20:00")),
-        ..Default::default()
-    });
-
-    mock_event.insert(String::from("participants"), AttributeValue {
-        ss: Some(vec![String::from("fisken"), String::from("krkl")]),
-        ..Default::default()
-    });
-
     let db_client = DynamoDbClient::new(Region::EuCentral1);
-    let db_input = PutItemInput {
-        item: mock_event,
-        table_name: String::from("disco-gaben-events"),
-        ..Default::default()
-    };
 
-    match db_client.put_item(db_input).sync() {
-        Ok(output) => {
-            println!("Output: {:?}", output);
-        }
-        Err(error) => {
-            println!("Error: {:?}", error);
-        }
-    }
     // Login with a bot token from the environment
     let mut discord_client = Client::new(&env::var("DISCORD_TOKEN").expect("token"), Handler)
         .expect("Error creating client");
 
     {
         let mut data = discord_client.data.lock();
-        data.insert::<EventList>(HashMap::default());
+        data.insert::<DbClient>(db_client);
     }
 
     discord_client.with_framework(StandardFramework::new()
@@ -101,8 +56,8 @@ fn main() {
                 .allowed_roles(vec!["Glorious leader", "pay2win"])
                 .cmd(event_add)
             )
-            .cmd("list", event_list)
-            .cmd("join", event_join)
+            // .cmd("list", event_list)
+            // .cmd("join", event_join)
         ));
 
     // start listening for events by starting a single shard
@@ -113,36 +68,66 @@ fn main() {
 
 command!(event_add(ctx, message, args) {
     let mut data = ctx.data.lock();
-    let events = data.get_mut::<EventList>().unwrap();
-    let id = events.len() as u64 + 1;
-    let event = Event {
-        id,
-        date: Utc.datetime_from_str(&args.single::<String>().unwrap(), "%Y-%m-%dT%H:%M").unwrap(),
-        title: args.single_quoted::<String>().unwrap(),
-        link: args.single::<String>().unwrap(),
-        participants: vec![message.author.clone()]
+    let db_client = data.get_mut::<DbClient>().unwrap();
+    let mut event: HashMap<String, AttributeValue> = HashMap::new();
+
+    event.insert(String::from("id"), AttributeValue {
+        s: Some(Uuid::new_v4().to_string()),
+        ..Default::default()
+    });
+
+    event.insert(String::from("date"), AttributeValue {
+        s: Some(args.single::<String>().unwrap()),
+        ..Default::default()
+    });
+
+    event.insert(String::from("title"), AttributeValue {
+        s: Some(args.single_quoted::<String>().unwrap()),
+        ..Default::default()
+    });
+
+    event.insert(String::from("link"), AttributeValue {
+        s: Some(args.single::<String>().unwrap()),
+        ..Default::default()
+    });
+
+    event.insert(String::from("participants"), AttributeValue {
+        ss: Some(vec![message.author.name.clone()]),
+        ..Default::default()
+    });
+
+    let db_input = PutItemInput {
+        item: event,
+        table_name: String::from("disco-gaben-events"),
+        ..Default::default()
     };
-    println!("{:?}", event);
-    events.insert(id, event);
-    let _ = message.reply("Added event");
-});
 
-command!(event_list(ctx, message) {
-    let mut data = ctx.data.lock();
-    let events = data.get_mut::<EventList>().unwrap();
-
-    for (_, event) in events.iter() {
-        let user_names: Vec<&String> = event.participants.iter().map(|u| &u.name).collect();
-        let msg = format!(":date: **{}** [#{}]\n`{}`\n{}\n```{:?}```", event.title, event.id, event.date, event.link, user_names);
-        let _ = message.channel_id.say(&msg);
+    match db_client.put_item(db_input).sync() {
+        Ok(_) => {
+            let _ = message.channel_id.say("Event has been added");
+        }
+        Err(error) => {
+            let _ = message.channel_id.say(format!("Failed adding event \n {}", error));
+        }
     }
 });
 
-command!(event_join(ctx, message, args) {
-    let mut data = ctx.data.lock();
-    let events = data.get_mut::<EventList>().unwrap();
+// command!(event_list(ctx, message) {
+//     let mut data = ctx.data.lock();
+//     let events = data.get_mut::<EventList>().unwrap();
 
-    let event = events.get_mut(&args.single::<u64>().unwrap()).unwrap();
-    event.participants.push(message.author.clone());
-    let _ = message.reply("Added you to event");
-});
+//     for (_, event) in events.iter() {
+//         let user_names: Vec<&String> = event.participants.iter().map(|u| &u.name).collect();
+//         let msg = format!(":date: **{}** [#{}]\n`{}`\n{}\n```{:?}```", event.title, event.id, event.date, event.link, user_names);
+//         let _ = message.channel_id.say(&msg);
+//     }
+// });
+
+// command!(event_join(ctx, message, args) {
+//     let mut data = ctx.data.lock();
+//     let events = data.get_mut::<EventList>().unwrap();
+
+//     let event = events.get_mut(&args.single::<u64>().unwrap()).unwrap();
+//     event.participants.push(message.author.clone());
+//     let _ = message.reply("Added you to event");
+// });
