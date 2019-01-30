1ed740ebd8a03cb9d6a7e169f207595cf575e4c9
Markus Warne <markus.warne@gmail.com>
2019-01-30 21:50:05 +0100

    adding user compatible with db

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ use chrono::prelude::*;
 use typemap::Key;
 use std::{env, collections::HashMap, default::Default};
 use rusoto_core::Region;
-use rusoto_dynamodb::{DynamoDb, DynamoDbClient, AttributeValue, PutItemInput};
+use rusoto_dynamodb::{DynamoDb, DynamoDbClient, AttributeValue, PutItemInput, UpdateItemInput};
 
 struct Handler;
 
@@ -57,7 +57,7 @@ fn main() {
                 .cmd(event_add)
             )
             // .cmd("list", event_list)
-            // .cmd("join", event_join)
+            .cmd("join", event_join)
         ));
 
     // start listening for events by starting a single shard
@@ -107,7 +107,7 @@ command!(event_add(ctx, message, args) {
             let _ = message.channel_id.say("Event has been added");
         }
         Err(error) => {
-            let _ = message.channel_id.say(format!("Failed adding event \n {}", error));
+            let _ = message.channel_id.say(format!("Failed adding event\n{}", error));
         }
     }
 });
@@ -123,11 +123,38 @@ command!(event_add(ctx, message, args) {
 //     }
 // });
 
-// command!(event_join(ctx, message, args) {
-//     let mut data = ctx.data.lock();
-//     let events = data.get_mut::<EventList>().unwrap();
+command!(event_join(ctx, message, args) {
+    let mut data = ctx.data.lock();
+    let db_client = data.get_mut::<DbClient>().unwrap();
 
-//     let event = events.get_mut(&args.single::<u64>().unwrap()).unwrap();
-//     event.participants.push(message.author.clone());
-//     let _ = message.reply("Added you to event");
-// });
+    let mut key = HashMap::new();
+    let mut exp_val = HashMap::new();
+
+    key.insert(String::from("id"), AttributeValue {
+        s: Some(args.single::<String>().unwrap()),
+        ..Default::default()
+    });
+
+    exp_val.insert(String::from(":user"), AttributeValue {
+        ss: Some(vec![message.author.name.clone()]),
+        ..Default::default()
+    });
+
+    let db_update = UpdateItemInput {
+        key,
+        expression_attribute_values: Some(exp_val),
+        update_expression: Some(String::from("ADD participants :user")),
+        table_name: String::from("disco-gaben-events"),
+        ..Default::default()
+    };
+
+    match db_client.update_item(db_update).sync() {
+        Ok(_) => {
+            let _ = message.channel_id.say("Added you to event");
+        }
+        Err(error) => {
+            let _ = message.channel_id.say(format!("Failed adding you to the event\n{}", error));
+            println!("Failed adding user to the event\n{}", error);
+        }
+    }
+});
