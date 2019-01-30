26e0d66ed1a18d5cc898b1015f40faf27da3efec
Markus Warne <markus.warne@gmail.com>
2019-01-30 23:06:23 +0100

    make event list work with db

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,20 @@
 #[macro_use] extern crate serenity;
 extern crate kankyo;
-extern crate chrono;
 extern crate typemap;
 extern crate rusoto_core;
 extern crate rusoto_dynamodb;
 extern crate uuid;
 
 use uuid::Uuid;
-use serenity::{client::Client, prelude::EventHandler, framework::standard::StandardFramework, model::user::User};
-use chrono::prelude::*;
+use serenity::{client::Client, prelude::EventHandler, framework::standard::StandardFramework};
 use typemap::Key;
 use std::{env, collections::HashMap, default::Default};
 use rusoto_core::Region;
-use rusoto_dynamodb::{DynamoDb, DynamoDbClient, AttributeValue, PutItemInput, UpdateItemInput};
+use rusoto_dynamodb::{DynamoDb, DynamoDbClient, AttributeValue, PutItemInput, UpdateItemInput, ScanInput};
 
 struct Handler;
 
 impl EventHandler for Handler { }
-
-#[derive(Debug)]
-struct Event {
-    id: u64,
-    date: DateTime<Utc>,
-    title: String,
-    link: String,
-    participants: Vec<User>,
-}
 
 struct DbClient;
 
@@ -56,7 +45,7 @@ fn main() {
                 .allowed_roles(vec!["Glorious leader", "pay2win"])
                 .cmd(event_add)
             )
-            // .cmd("list", event_list)
+            .cmd("list", event_list)
             .cmd("join", event_join)
         ));
 
@@ -112,16 +101,41 @@ command!(event_add(ctx, message, args) {
     }
 });
 
-// command!(event_list(ctx, message) {
-//     let mut data = ctx.data.lock();
-//     let events = data.get_mut::<EventList>().unwrap();
+command!(event_list(ctx, message) {
+    let mut data = ctx.data.lock();
+    let db_client = data.get_mut::<DbClient>().unwrap();
 
-//     for (_, event) in events.iter() {
-//         let user_names: Vec<&String> = event.participants.iter().map(|u| &u.name).collect();
-//         let msg = format!(":date: **{}** [#{}]\n`{}`\n{}\n```{:?}```", event.title, event.id, event.date, event.link, user_names);
-//         let _ = message.channel_id.say(&msg);
-//     }
-// });
+    let db_scan = ScanInput {
+        table_name: String::from("disco-gaben-events"),
+        ..Default::default()
+    };
+
+    match db_client.scan(db_scan).sync() {
+        Ok(result) => {
+            match result.items {
+                Some(items) => {
+                    for event in items.iter() {
+                        let id = event.get(&String::from("id")).unwrap().s.clone().unwrap();
+                        let date = event.get(&String::from("date")).unwrap().s.clone().unwrap();
+                        let title = event.get(&String::from("title")).unwrap().s.clone().unwrap();
+                        let link = event.get(&String::from("link")).unwrap().s.clone().unwrap();
+                        let participants = event.get(&String::from("participants")).unwrap().ss.clone().unwrap();
+
+                        let msg = format!(":date: **{}** [{}]\n`{}`\n{}\n```{:?}```", title, id, date, link, participants);
+                        let _ = message.channel_id.say(&msg);
+                    }
+                }
+                None => {
+                    let _ = message.channel_id.say(String::from("No events found"));
+                }
+            }
+        }
+        Err(error) => {
+            let _ = message.channel_id.say(format!("Failed adding you to the event\n{}", error));
+            println!("Failed adding user to the event\n{}", error);
+        }
+    }
+});
 
 command!(event_join(ctx, message, args) {
     let mut data = ctx.data.lock();
