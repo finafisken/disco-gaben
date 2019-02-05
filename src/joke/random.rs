076fce34c2a0daa2105c63b688757d57f16c3072
Benjamin Wallberg <benjamin.wallberg@nentgroup.com>
2019-02-05 14:45:22 +0100

    Simplify joke API usage by using text/plain accept header

--- src/joke/random.rs
+++ src/joke/random.rs
@@ -1,31 +1,23 @@
 extern crate reqwest;
 
 use reqwest::header::{ACCEPT};
-use std::collections::HashMap;
-use std::io::{Error, ErrorKind};
 
-fn fetch_joke() -> Result<(HashMap<String, String>), Box<std::error::Error>> {
+fn fetch_joke() -> Result<(String), Box<std::error::Error>> {
     let client = reqwest::Client::new();
     let mut response = client.get("https://icanhazdadjoke.com")
-        .header(ACCEPT, "application/json")
+        .header(ACCEPT, "text/plain")
         .send()?;
-    let json = response.json()?;        
-    Ok(json)
+    let joke = response.text()?;
+    Ok(joke)
 }
 
 command!(cmd(_ctx, message) {
     match fetch_joke() {
-        Ok(response) => {
-            match response.get("joke").ok_or(Error::new(ErrorKind::Other, "Joke not found!")) {
-                Ok(joke) => {
-                    message.channel_id.say(joke)?;
-                }
-                Err(_e) => {
-                    message.channel_id.say(String::from("Error, Gaben is sleeping on the job"))?;
-                }
-            }
+        Ok(joke) => {
+            message.channel_id.say(joke)?;
         }
-        Err(_e) => {
+        Err(e) => {
+            println!("[joke::random] Error {:?}", e);
             message.channel_id.say(String::from("Error, unforeseen consequences."))?;
         }
     }
